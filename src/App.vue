f8f35977b2e5dd52aa26b7d601a61aee20633e5d
David <60773817+gloriousky@users.noreply.github.com>
2022-01-26 17:37:19 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <img class="mx-auto mt-12 mb-8" alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <a href="">第一週作業</a>
+  <a href="">第二週作業</a>
+  <HelloWorld />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
